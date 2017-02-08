43a6d78789bc90271189d8c36aeaa910ea164975
Kasia <kasia@wiseman.pl>
2017-02-08 10:24:52 +0100

    Initial commit

---
+++ src/pl/javastart/zadanie2/DataServlet.java
@@ -0,0 +1,65 @@
+package pl.javastart.zadanie2;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Kasia on 06.02.2017.
+ */
+@WebServlet("/DataServlet")
+public class DataServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = createUserFromRequest(request);
+        sendResponse(user, response);
+    }
+    private User createUserFromRequest(HttpServletRequest request) {
+        User user = new User();
+        user.setFirstName(request.getParameter("imię"));
+        user.setLastName(request.getParameter("nazwisko"));
+        user.setAge(request.getParameter("wiek"));
+        user.setUrl(request.getParameter("url"));
+        return user;
+    }
+
+    private void sendResponse(User user, HttpServletResponse response) throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.print("<div>");
+        if ( user.getFirstName() != null && user.getLastName() != null){
+            writer.println("<h3>Przyjęto następujące dane</h3>");
+            writer.println(user.getFirstName() + "<br>");
+            writer.println(user.getLastName() + "<br>");
+            writer.println(user.getAge() + "<br>");
+            writer.println(user.getUrl() + "<br>");
+            writer.println("<img src=\"http://www.fantastycznie.pl/upload/images/large/2015/03/smieszny_kot_2015-03-15_19-51-18.jpg\">");
+            writer.println("</div>");
+            writer.println("</body>");
+            writer.println("</html>");
+        }else{
+            writer.println("<h3>Wprowadzono błędne dane</h3>");
+            writer.println("<h4><a href=http://localhost:8081/index1.html>powrót do formularza</a></h4>");
+
+        }
+    }
+
+
+    public DataServlet() {
+        super();
+    }
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = createUserFromRequest(request);
+        sendResponse(user, response);
+    }
+
+
+}
---
+++ src/pl/javastart/zadanie2/User.java
@@ -0,0 +1,64 @@
+package pl.javastart.zadanie2;
+
+/**
+ * Created by Kasia on 06.02.2017.
+ */
+public class User {
+    private String firstName;
+    private String lastName;
+    private String age;
+    private String url;
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (firstName != null ? !firstName.equals(user.firstName) : user.firstName != null) return false;
+        return lastName != null ? lastName.equals(user.lastName) : user.lastName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
+    }
+}
